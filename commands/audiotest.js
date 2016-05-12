838f8663c454f77b1d1fdb170a2218367732b2a1
Michael Albertz <michael.albertz@web.de>
2016-05-12 21:11:13 +0200

    added an audiotest using discord.js

--- commands/audiotest.js
+++ commands/audiotest.js
@@ -2,5 +2,17 @@ var path = require('path');
 
 
 exports.run = function(bots, commands, message, args) {
-
+  bots.js.joinVoiceChannel(message.author.voiceChannel, (err, connection) => {
+    if (err) {
+        bots.js.sendMessage(message.channel, err);
+        return;
+    }
+    bots.js.sendMessage(message.channel, "hello!");
+    setTimeout(function () {
+      connection.playFile('../media/audio/toni.mp3', () => {
+        bots.js.sendMessage(message.channel, "bye.");
+        bots.js.leaveVoiceChannel(connection.voiceChannel);
+      });
+    }, 2000);
+  });
 };
