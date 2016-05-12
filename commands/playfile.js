4dfe88f7732c67c04d55d4b4c173fc379ee03cb1
Michael Albertz <michael.albertz@web.de>
2016-05-12 22:19:28 +0200

    no io bot is needed anymore. the js bod cant leave the channel by himself yet though :(

--- commands/playfile.js
+++ commands/playfile.js
@@ -2,47 +2,61 @@ var queue = [];
 var playing = false;
 
 function play(bots, message, file) {
-  var channelID = message.author.voiceChannel.id;
-  var io_bot_voice_id = bots.io._bot.servers[message.channel.server.id].members[bots.io._bot.id].voice_channel_id;
+  var channel = message.author.voiceChannel;
+  var currentChannel = bots.js.user.voiceChannel;
 
   if (file)
     queue.push(file);
 
-  if (channelID != io_bot_voice_id) {
-    bots.io._bot.joinVoiceChannel(channelID, () => {
-      start(bots, channelID);
+  if (channel != currentChannel) {
+    bots.js.joinVoiceChannel(message.author.voiceChannel, (err, connection) => {
+      if (err) throw err;
+      start(bots, message, connection);
     });
   } else {
     setTimeout(function () {
-      start(bots, channelID);
+      console.log(bots.js);
+      start(bots, message, bots.js.voiceConnection);
     }, 300);
   }
 
 }
 
-function start(bots, channelID) {
+function start(bots, message, connection) {
   if (playing) return;
-  play_i(bots, channelID, queue.shift(), next);
-  function next() {
-      if (queue.length > 0) {
-        play_i(bots, channelID, queue.shift(), next);
-      } else {
-        bots.io._bot.leaveVoiceChannel(channelID);
-      }
+  play_i(connection, queue.shift(), next);
+  function next(e) {
+    if (e) {
+      bots.js.sendMessage(message.channel, `\`\`\`${e.stack}\`\`\``);
+    }
+    if (queue.length > 0) {
+      play_i(connection, queue.shift(), next);
+    } else {
+      //bots.js.leaveVoiceChannel(bots.js.user.voiceChannel, err => {
+      //  if (err) throw err;
+      //});
+    }
   }
 }
 
-function play_i(bots, channelID, file, callback) {
+function play_i(connection, file, callback) {
   playing = true;
-  bots.io._bot.getAudioContext( { channel: channelID, stereo: true }, (stream) => {
-    stream.playAudioFile(file);
-    stream.once('fileEnd', () => {
-      playing = false;
-      setTimeout(function () {
-        callback();
-      }, 500);
+  try {
+    connection.playFile(file, (err, intent) => { //http://www.noiseaddicts.com/samples_1w72b820/3740.mp3
+      if (err) throw err;
+      intent.on('error',  err => {
+        throw err;
+      });
+      intent.on('end',  () => {
+        playing = false;
+        callback(null);
+      });
     });
-  });
+  } catch (e) {
+    playing = false;
+    callback(e);
+  }
+
 
 }
 
