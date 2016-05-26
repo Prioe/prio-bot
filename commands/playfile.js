2b89e09e78ed0b44df1fe6db3293a7f60c8c5309
Michael Albertz <michael.albertz@web.de>
2016-05-26 14:12:59 +0200

    new version of discord.js fixed the bot not being able to leave the voice channel. the play file implementation is pretty solid and error proof

--- commands/playfile.js
+++ commands/playfile.js
@@ -2,48 +2,98 @@ var queue = [];
 var playing = false;
 
 function play(bots, message, file) {
-  var channelID = message.author.voiceChannel.id;
-  var io_bot_voice_id = bots.io._bot.servers[message.channel.server.id].members[bots.io._bot.id].voice_channel_id;
-
-  if (file)
-    queue.push(file);
-
-  if (channelID != io_bot_voice_id) {
-    bots.io._bot.joinVoiceChannel(channelID, () => {
-      start(bots, channelID);
-    });
-  } else {
-    setTimeout(function () {
-      start(bots, channelID);
-    }, 300);
+  if (!file) {
+    bots.js.sendMessage(message.channel, help(''));
+    return;
   }
 
-}
+  var channel = message.author.voiceChannel;
+  queue.push({ target: channel, file: file});
 
-function start(bots, channelID) {
   if (playing) return;
-  play_i(bots, channelID, queue.shift(), next);
-  function next() {
-      if (queue.length > 0) {
-        play_i(bots, channelID, queue.shift(), next);
-      } else {
-        bots.io._bot.leaveVoiceChannel(channelID);
-      }
-  }
+  playQueue(bots.js);
+
 }
 
-function play_i(bots, channelID, file, callback) {
+function playQueue(bot) {
   playing = true;
-  bots.io._bot.getAudioContext( { channel: channelID, stereo: true }, (stream) => {
-    stream.playAudioFile(file);
-    stream.once('fileEnd', () => {
+  var currentChannel = bot.user.voiceChannel;
+  next();
+
+  function next() {
+    if (queue.length <= 0) {
+      bot.leaveVoiceChannel(currentChannel).catch(function (e) {
+        playing = false;
+        console.log('something bad happened while leaving the voice channel:\n' + e.stack);
+      }).then(function () {
+        playing = false;
+        console.log(`left voice channel '${currentChannel}'`);
+      });
+      return;
+    }
+    var currentFile = queue.shift();
+    if (currentChannel !== null && currentChannel === currentFile.target) {
+      //were already in the right channel, just play the file
+      playInCurrentChannel(bot, currentFile, next);
+    } else if (currentChannel !== null && currentChannel != currentFile.target) {
+      //were in a channel, but the wrong one. leave this channel and join the right one, then play the file
+      bot.leaveVoiceChannel(bot.user.voiceChannel).catch(function (e) {
+        throw e;
+      }).then(function () {
+        console.log(`left voice channel '${currentChannel}'`);
+        currentChannel = null;
+        bot.joinVoiceChannel(currentFile.target).catch(function (e) {
+          throw e;
+        }).then(function (connection) {
+          currentChannel = bot.user.voiceChannel;
+          console.log(`joined voice channel '${currentChannel}'`);
+          playInCurrentChannel(bot, currentFile, next);
+        });
+      });
+
+
+    } else if (currentChannel === null) {
+      //were in no channel, just join the right one and play the file
+      bot.joinVoiceChannel(currentFile.target).catch(function (e) {
+        throw e;
+      }).then(function (connection) {
+        currentChannel = bot.user.voiceChannel;
+        console.log(`joined voice channel '${currentChannel}'`);
+        playInCurrentChannel(bot, currentFile, next);
+      });
+    } else {
+      //something really unexpected and bad happened :/. the target channel is null
+      bot.sendMessage(message.channel,
+`\`\`\`
+this should never happen! :(
+  target: ${channel}
+  current: ${currentChannel}
+  queue: ${queue}
+\`\`\``
+      );
+    }
+  }
+
+}
+
+function playInCurrentChannel(bot, file, callback) {
+  console.log(`playing '${file.file}'`);
+  bot.voiceConnection.playFile(file.file, (err, intent) => { //http://www.noiseaddicts.com/samples_1w72b820/3740.mp3
+    if (err) throw err;
+    intent.on('error',  err => {
+      throw err;
+    });
+    intent.on('end',  () => {
       playing = false;
-      setTimeout(function () {
-        callback();
-      }, 500);
+      callback();
     });
   });
+}
 
+function help(prefix) {
+  return `\`\`\`${prefix}playfile <FILE>
+    play a file (http-hosted or local) in your voice channel
+    //TODO: play file from attachement\`\`\``;
 }
 
 exports.run = function(bots, commands, message, args) {
@@ -51,9 +101,4 @@ exports.run = function(bots, commands, message, args) {
 };
 
 exports.play = play;
-
-exports.help = function (prefix) {
-  return `\`\`\`${prefix}playfile <FILE>
-    play a file (http-hosted or local) in your voice channel
-    //TODO: play file from attachement\`\`\``;
-};
+exports.help = help;
