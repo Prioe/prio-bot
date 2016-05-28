c5cedf5a55f3821601adca80e7cb3662c34305d4
Michael Albertz <michael.albertz@web.de>
2016-05-28 22:31:44 +0200

    ripeb: better style

--- commands/ripeb.js
+++ commands/ripeb.js
@@ -1,14 +1,15 @@
 var play = require('../commands/playfile.js');
 var path = require('path');
-var lastplayed;
-var COOLDOWN = 5;
+var lastplayed = 0;
+const COOLDOWN = 5;
 var settings = require('../settings.json');
 
 exports.run = function(bots, commands, message, args) {
-  if ((lastplayed && lastplayed > new Date().getTime()) && !settings.sudo_role) {
-    bots.js.reply(message, `NOCH ${Math.floor((lastplayed - new Date().getTime()) / (60 *1000))} MINUTEN COOLDOWN :ok_hand::ok_hand::ok_hand::ok_hand::ok_hand::ok_hand::ok_hand::ok_hand:`);
+  var currentTime = new Date().getTime();
+  if ((lastplayed > currentTime) && !settings.sudo_role) {
+    bots.js.reply(message, `NOCH ${Math.floor((lastplayed - currentTime) / (60 *1000))} MINUTEN COOLDOWN :ok_hand::ok_hand::ok_hand::ok_hand::ok_hand::ok_hand::ok_hand::ok_hand:`);
     return;
   }
   play.play(bots, message, path.join(__dirname, '../media/audio/breakeb.mp3'));
-  lastplayed = new Date().getTime() + COOLDOWN*60*1000;
+  lastplayed = currentTime + COOLDOWN*60*1000;
 };
